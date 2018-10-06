efd1a0bcab9145bed203b18cc99506fef1c4ba6a
ShruthiSudharsan <42788894+ShruthiSudharsan@users.noreply.github.com>
2018-10-06 14:53:05 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,31 +1,27 @@
-# Shruthi, make me a number guessing game
+# Random Number Guessing Game by Shruthi Sudharsan- a rockstar coding student
+# at theCoderSchool in Flower Mound, TX
 
-# CODE EXAMPLES FOR REFERENCE:
-'''
-print("THIS IS HOW YOU PRINT TEXT")
-
-userInput = input("PROMPT USER TO ENTER INPUT> ")
-
-# Generate A Random Number Between 0 and 13
-import random
-randomNumber = random.randint(0, 13)
-
-'''
 import random
 
+print("guess.py")
+print("Copyleft 2018, Shruthi Sudharsan")
+print("")
 print("Guess a number between 0 and 13")
-print(" ")
+print("")
 
 randomNumber = random.randint(0, 13)
+guess = False
 
-guess = input("Guess number here> ")
-guess = int(guess) # cast as integer
-
-if randomNumber == guess:
-    print("Correct!")
-elif randomNumber > guess:
-    print("Guess higher")
-elif randomNumber < guess:
-    print("Guess lower")
-else:
-    print("Error guess again")
+while guess != randomNumber:
+    # PROMPT USER
+    guess = input("Guess number here> ")
+    guess = int(guess) # cast as integer
+    # DETERMINE OUTCOME
+    if randomNumber == guess:
+        print("Correct!")
+    elif randomNumber > guess:
+        print("Guess higher")
+    elif randomNumber < guess:
+        print("Guess lower")
+    else:
+        print("Error guess again")
