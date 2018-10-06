32b9c6810775857a9cbcbd9fd1c7a61bf7649377
ShruthiSudharsan <42788894+ShruthiSudharsan@users.noreply.github.com>
2018-10-06 14:43:46 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,21 +1,31 @@
 # Shruthi, make me a number guessing game
 
-'''
 # CODE EXAMPLES FOR REFERENCE:
-
-# PRINT TEXT
+'''
 print("THIS IS HOW YOU PRINT TEXT")
 
-# PROMPT USER FOR INPUT
 userInput = input("PROMPT USER TO ENTER INPUT> ")
 
-# GENERATE A RANDOM NUMBER BETWEEN 0 AND 13
+# Generate A Random Number Between 0 and 13
 import random
 randomNumber = random.randint(0, 13)
 
 '''
+import random
 
 print("Guess a number between 0 and 13")
 print(" ")
 
-userinput = input("Guess number here> ")
+randomNumber = random.randint(0, 13)
+
+guess = input("Guess number here> ")
+guess = int(guess) # cast as integer
+
+if randomNumber == guess:
+    print("Correct!")
+elif randomNumber > guess:
+    print("Guess higher")
+elif randomNumber < guess:
+    print("Guess lower")
+else:
+    print("Error guess again")
