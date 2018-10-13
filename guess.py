f0672e98c87c6efc9512f2905b39e3bc083bf545
ShruthiSudharsan <42788894+ShruthiSudharsan@users.noreply.github.com>
2018-10-13 14:24:24 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,26 +1,36 @@
-# Random Number Guessing Game by Shruthi Sudharsan
+# Random Number Guessing Game by Shruthi Sudharsan- a coding student
+# at theCoderSchool in Flower Mound, TX
 
 import random
 
 print("guess.py")
 print("Copyleft 2018, Shruthi Sudharsan")
 print("")
-print("Guess a number between 0 and 13")
-print("")
 
-randomNumber = random.randint(0, 13)
-guess = False
+keepPlaying = True
+while keepPlaying == True:
+    print("Guess a number between 0 and 100")
+    print("")
 
-while guess != randomNumber:
-    # PROMPT USER
-    guess = input("Guess number here> ")
-    guess = int(guess) # cast as integer
-    # DETERMINE OUTCOME
-    if randomNumber == guess:
-        print("Correct!")
-    elif randomNumber > guess:
-        print("Guess higher")
-    elif randomNumber < guess:
-        print("Guess lower")
+    randomNumber = random.randint(0, 100)
+    guess = False
+
+    while guess != randomNumber:
+        # PROMPT USER
+        guess = input("Guess number here> ")
+        guess = int(guess) # cast as integer
+        # DETERMINE OUTCOME
+        if randomNumber == guess:
+            print("Correct!")
+        elif randomNumber > guess:
+            print("Guess higher")
+        elif randomNumber < guess:
+            print("Guess lower")
+        else:
+            print("Error guess again")
+
+    keepPlaying = input("Would you like to play again? Enter 'y' for yes, 'n' for no.")
+    if keepPlaying == "y":
+        keepPlaying = True
     else:
-        print("Error guess again")
+        keepPlaying = False
